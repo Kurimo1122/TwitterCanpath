692aa2bfd5d687f6570b21186ebb63d9fd27a698
Toshimitsu Kugimoto <kugimoto1122@gmail.com>
2016-12-26 02:18:00 +0900

    Initial Commit

---
+++ TwitterCanpath/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TwitterCanpath
+//
+//  Created by Toshimitsu Kugimoto on 2016/12/26.
+//  Copyright © 2016 Toshimitsu Kugimoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
